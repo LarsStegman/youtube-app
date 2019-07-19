d360e844e31ca53760d884ef0282d4edfe39e107
Lars Stegman <l.s.stegman@student.tudelft.nl>
2019-07-19 20:48:30 +0200

    Initial Commit

---
+++ MyTubePlayer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MyTubePlayer
+//
+//  Created by Lars Stegman on 19/07/2019.
+//  Copyright © 2019 Stegman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
